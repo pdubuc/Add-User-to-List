61ff49dedc06de86309d0b2ed3289733ff5e7f5c
“pdubuc” <pd21248@gmail.com>
2023-12-28 09:59:26 -0800

    Sync with VS code

--- src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import classes from "./Card.module.css";
-
-const Card = (props) => {
-    // Lesson 124. How to apply 2 types of classes - 
-    //  a) internal to Card & b) coming in externally
-    return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
-};
-
-export default Card;
